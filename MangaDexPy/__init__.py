792a52218946081331fdf06c64366bebc906ead3
Proxymiity <commits@proxymiity.fr>
2021-03-11 19:25:34 +0100

    Added the base login method

--- MangaDexPy/__init__.py
+++ MangaDexPy/__init__.py
@@ -1,3 +1,36 @@
+import requests
+from .manga import Manga
+
+
 class MangaDex:
     def __init__(self):
-        return
+        self.url = "https://mangadex.org"
+        self.api = "https://api.mangadex.org/v2"
+        self.session = requests.Session()
+        self.user = None
+        self.login_success = False
+
+    def login(self, username: str, password: str):
+        url = "{}/ajax/actions.ajax.php?function=login".format(self.url)
+        credentials = {"login_username": username, "login_password": password}
+        headers = {
+            "method": "POST",
+            "path": "/ajax/actions.ajax.php?function=login",
+            "scheme": "https",
+            "Accept": "*/*",
+            "accept-encoding": "gzip, deflate, br",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "origin": self.url,
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+        }
+
+        post = self.session.post(url, data=credentials, headers=headers)
+        if not post.cookies.get("mangadex_session"):
+            print("Invalid MangaDex credentials. Some API calls will be unavailable.")
+            return False
+        else:
+            self.login_success = True
+            return True
