6bf465f2bf3cd422f41bf8c94447a69893dbe3c1
Proxymiity <commits@proxymiity.fr>
2021-03-18 09:00:49 +0100

    Updated the setup.py and python requirements

--- setup.py
+++ setup.py
@@ -7,17 +7,34 @@ setup(name='MangaDex.py',
       packages=[
           "MangaDexPy"
       ],
-      version='1.0.1',
+      version='1.0.2',
       description='An API wrapper for MangaDex.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/Proxymiity/MangaDex.py',
       download_url='https://github.com/Proxymiity/MangaDex.py/releases',
+      changelog='https://github.com/Proxymiity/MangaDex.py/releases',
+      documentation='https://github.com/Proxymiity/MangaDex.py/wiki',
+      source='https://github.com/Proxymiity/MangaDex.py/tree/main/MangaDexPy',
       author='Proxymiity',
       author_email='commits@proxymiity.fr',
       license='MIT',
       install_requires=[
-          'requests>=2.25.0',
+            'requests>=2.25.0',
       ],
-      zip_safe=False
+      classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Topic :: Internet',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Utilities',
+            'Intended Audience :: Developers'
+      ],
+      zip_safe=False,
+      python_requires='>=3.6'
       )
