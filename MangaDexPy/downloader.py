e2c45490bf015f9b8214f0328ffd2e8f58b040c0
Proxymiity <commits@proxymiity.fr>
2021-05-14 23:49:40 +0200

    Updated the downloader

--- MangaDexPy/downloader.py
+++ MangaDexPy/downloader.py
@@ -3,17 +3,22 @@ import time
 from pathlib import Path
 
 
-def dl_page(chapter: Chapter, page, path, fallback=False):
+def dl_page(net, page, path):
     """Downloads a single page."""
-    with chapter.session.get(chapter.format_url(page, fallback)) as p:
-        with open(str(Path(path + "/" + page)), "wb") as f:
+    with net.client.session.get(page) as p:
+        with open(str(Path(path + "/" + page.rsplit("/", 1)[1])), "wb") as f:
             f.write(p.content)
+        success = True if p.status_code <= 400 else False
+        cached = True if p.headers["x-cache"] == "HIT" else False
+        net.report(page, success, cached, len(p.content), int(p.elapsed.microseconds/1000))
+        print(f"Statistics for {page}\nTime: {int(p.elapsed.microseconds/1000)}, length: {len(p.content)}"
+              f"\nSuccess: {success}, was cached on server: {cached}\nHeaders: {p.headers}")
 
 
-def dl_chapter(chapter: Chapter, path, fallback=False, time_controller: int = 1):
+def dl_chapter(chapter: Chapter, path, time_controller: int = 1):
     """Downloads an entire chapter."""
-    chapter.refresh()
-    for x in chapter.pages:
-        dl_page(chapter, x, path, fallback)
+    net = chapter.get_md_network()
+    for x in net.pages:
+        dl_page(net, x, path)
         if time_controller:
             time.sleep(time_controller)
